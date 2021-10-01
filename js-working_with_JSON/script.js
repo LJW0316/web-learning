b04378638ead235113c6d15dd51e8879f7b7e341
LJW0316 <lijingwei010316@163.com>
2021-10-01 18:55:46 +0800

    2021.10.1

--- js-working_with_JSON/script.js
+++ js-working_with_JSON/script.js
@@ -2,14 +2,26 @@ const header = document.querySelector('header');
 const section = document.querySelector('section');
 let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
+// request.send()
+// request.onload = function () {
+//     let  superHeroes = request.response;
+//     populateHeader(superHeroes);
+//     showHeroes(superHeroes);
+// }
+
 request.open('GET', requestURL);
-request.responseType = 'json';
-request.send()
+request.responseType = 'text';
+request.send();
+
 request.onload = function () {
-    let  superHeroes = request.response;
+    let superHeroesText = request.response;
+    let superHeroes = JSON.parse(superHeroesText);
     populateHeader(superHeroes);
     showHeroes(superHeroes);
 }
+
 
 function populateHeader(jsonObj) {
     let myH1 = document.createElement('h1');
